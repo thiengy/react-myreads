4dbd21cb5485de0252082cb57133e3ea839c4bc3
thiengy <thiengy@protonmail.com>
2017-11-19 21:32:47 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,35 +4,25 @@ import './App.css'
 import BookShelf from './BookShelf'
 import BookSearch from './BookSearch'
 
-class App extends Component {
-
-  render() {
-    return(
-      <div className="app">
-        <Route exact path='/' render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-              <BookShelf />
-            <div className="open-search">
-              <Link to="/search">Add Book</Link>
-            </div>
+function App (props) {
+  return(
+    <div className="app">
+      <Route exact path='/' render={() => (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
           </div>
-        )}/>
-        <Route exact path='/search' render={() => (
-          <BookSearch />
-        )}/> 
-      </div>
-    )
-  }
+            <BookShelf />
+          <div className="open-search">
+            <Link to="/search">Add Book</Link>
+          </div>
+        </div>
+      )}/>
+      <Route exact path='/search' render={() => (
+        <BookSearch />
+      )}/> 
+    </div>
+  )
 }
 
 export default App;
-
-
-// Need to enable backend API
-// Need to setup some action to modify state from the child.
-
-
-// parent function will call set.state <button onClick={(App.moveBook())} />
