17649834f20cad651cdbb08ef5c6ad4b6dc5b4ad
thiengy <thiengy@protonmail.com>
2017-11-21 21:08:14 -0700

    Add files via upload
    Moved state to app.js so it could pass to all child components to fix search results.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
 import Book from './Book'
-import * as BooksAPI from './BooksAPI'
 
 class BookShelf extends Component {
-  state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: [],
-  }
-        
-  loadBooks() {
-    BooksAPI.getAll().then((books) => {
-        let shelfCurrentlyReading = books.filter((book) => {return book.shelf === 'currentlyReading'})
-        let shelfWantToRead = books.filter((book) => {return book.shelf === 'wantToRead'})
-        let shelfRead = books.filter((book) => {return book.shelf === 'read'})
-
-        this.setState({
-        currentlyReading: shelfCurrentlyReading,
-        wantToRead: shelfWantToRead,
-        read: shelfRead
-        })
-    })
-  }
-  
-  reloadBooks() {
-    this.loadBooks()
+  constructor(props){
+    super(props)
+    this.state = this.props
   }
 
-  componentDidMount(){
-    this.loadBooks()
-  }
-        
   render() {
     return(
       <div>
@@ -41,9 +17,11 @@ class BookShelf extends Component {
                     <h2 className="bookshelf-title">Currently Reading</h2>
                     <div className="bookshelf-books">
                         <ol className="books-grid">
-                          {this.state.currentlyReading.map((book) => (
-                            <Book book={book} key={book.id} reloadBooks={this.reloadBooks.bind(this)} />
-                          ))}
+                          {this.props.books
+                            .filter((book) => book.shelf === 'currentlyReading')
+                              .map((book) => (
+                                <Book book={book} key={book.id} reloadBooks={this.props.reloadBooks} />
+                            ))}
                         </ol>
                     </div>
                 </div>
@@ -55,9 +33,11 @@ class BookShelf extends Component {
                     <h2 className="bookshelf-title">Want to Read</h2>
                     <div className="bookshelf-books">
                         <ol className="books-grid">
-                        {this.state.wantToRead.map((book) => (
-                          <Book book={book} key={book.id} reloadBooks={this.reloadBooks.bind(this)} />
-                        ))}
+                          {this.props.books
+                              .filter((book) => book.shelf === 'wantToRead')
+                                .map((book) => (
+                                  <Book book={book} key={book.id} reloadBooks={this.props.reloadBooks} />
+                              ))}
                       </ol>
                     </div>
                 </div>
@@ -69,9 +49,11 @@ class BookShelf extends Component {
                     <h2 className="bookshelf-title">Read</h2>
                     <div className="bookshelf-books">
                         <ol className="books-grid">
-                        {this.state.read.map((book) => (
-                            <Book book={book} key={book.id} reloadBooks={this.reloadBooks.bind(this)} />
-                          ))}
+                          {this.props.books
+                              .filter((book) => book.shelf === 'read')
+                                .map((book) => (
+                                  <Book book={book} key={book.id} reloadBooks={this.props.reloadBooks} />
+                              ))}
                         </ol>
                     </div>
                 </div>
