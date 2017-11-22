a305f0a7dea1a5d24479347d010fb941d321be50
thiengy <thiengy@protonmail.com>
2017-11-21 21:04:44 -0700

    Add files via upload
    Moved state to app.js so it could pass to child components.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import './App.css'
 import BookShelf from './BookShelf'
 import BookSearch from './BookSearch'
+import * as BooksAPI from './BooksAPI'
 
-function App (props) {
-  return(
-    <div className="app">
-      <Route exact path='/' render={() => (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
+class App extends Component {
+  state = {
+    books: []
+  }
+
+  loadBooks() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books: books
+      })
+    })
+  }
+  
+  reloadBooks() {
+    this.loadBooks()
+  }
+
+  componentDidMount(){
+    this.loadBooks()
+  }
+
+  render() {
+    return(
+      <div className="app">
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+              <BookShelf books={this.state.books} reloadBooks={this.reloadBooks.bind(this)} />
+            <div className="open-search">
+              <Link to="/search">Add Book</Link>
+            </div>
           </div>
-            <BookShelf />
-          <div className="open-search">
-            <Link to="/search">Add Book</Link>
-          </div>
-        </div>
-      )}/>
-      <Route exact path='/search' render={() => (
-        <BookSearch />
-      )}/> 
-    </div>
-  )
+        )}/>
+        <Route exact path='/search' render={() => (
+          <BookSearch books={this.state.books} reloadBooks={this.reloadBooks.bind(this)} />
+        )}/> 
+      </div>
+    )
+  }
 }
 
 export default App;
